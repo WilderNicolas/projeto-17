864f67b0af021aebf1c885d6cda782f39a406571
WilderNicolas <135550609+WilderNicolas@users.noreply.github.com>
2023-07-09 17:39:38 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ mainCyclist.scale=0.07;
 //set collider for mainCyclist
 
 //mainCyclist.setCollission("rectangle",0,0,40,40);
-//mainCyclist.setCollider("rectangle",0,0,40,40);
+mainCyclist.setCollider("rectangle",0,0,40,40);
 //mainCyclist.setCollission("rectangle",0,0,40,40,50);
 //mainCyclist.setCollider("rectangle",0,0,40,40,50);
 
@@ -145,9 +145,7 @@ function draw() {
     redCG.setVelocityXEach(0);
     redCG.setLifetimeEach(-1);
     
-    // if(keyDown("UP_ARROW")) {
-    //   reset;
-    // }
+   
 
     // if(key("UP_ARROW")) {
     //   reset();
@@ -157,9 +155,9 @@ function draw() {
     //   reset();
     // }
 
-    // if(keyDown("UP_ARROW")) {
-    //   reset();
-    // }
+     if(keyDown("UP_ARROW")) {
+       reset();
+     }
 }
 }
 
@@ -214,17 +212,17 @@ function redCyclists(){
 //  distance = 0;
 // }
 
-//function reset(){
-//  gameState = PLAY;
-//  gameOver.visible = false;
-//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+function reset(){
+ gameState = PLAY;
+  gameOver.visible = false;
+  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
   
-//  pinkCG.destroyEach();
-//  yellowCG.destroyEach();
-//  redCG.destroyEach();
+  pinkCG.destroyEach();
+  yellowCG.destroyEach();
+  redCG.destroyEach();
   
-//  distance = 0;
-// }
+  distance = 0;
+ }
 
 //function reset(){
 //  gameState = END;
